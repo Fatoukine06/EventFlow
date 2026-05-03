d5c02c5c9b234a688b494c6cc562f510add55e8b
Fatoukine06 <fatoukine.gye06@gmail.com>
2026-05-03 21:13:48 +0000

    Ajout de Create Events

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-
+import CreateEvent from "./components/Creation"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
 
 
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<CreateEvent />} />
+        </Routes>
+      </Router>
     </>
   )
 }
